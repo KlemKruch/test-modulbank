2725239d6932a601e74b4c436862484950e7c3b4
Klem <kerrigan.vig56@gmail.com>
2025-08-27 13:38:49 +0300

    creation and adding player

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,70 @@
 import { useMachine } from "@xstate/react";
 import { machine } from "./machine";
+import { CiPlay1, CiMinimize1, CiTurnR1, CiMaximize1 } from "react-icons/ci";
 import "./App.css";
 
 function App() {
-  const [state, send] = useMachine(machine);
+	const [state, send] = useMachine(machine);
 
-  const mini = state.matches("mini");
-  console.log(mini);
-  return (
-    <>
-      <div className="video-block">
-        <p>
-          Create a mini web application using the State Machines pattern to
-          manage the interface state.
-        </p>
-      </div>
-    </>
-  );
+	const mini = state.matches("mini");
+	const miniPlayer = state.matches("fullMini");
+
+	window.addEventListener("keydown", (event) => {
+		if (event.key === "Escape") {
+			send({ type: "key.escape" });
+		}
+	});
+
+	return (
+		<>
+			<div className="video-block">
+				<p>
+					Create a mini web application using the State Machines
+					pattern to manage the interface state.
+				</p>
+				<div className="mini-block">
+					<CiPlay1
+						className="button"
+						onClick={() => send({ type: "toggle" })}
+					/>
+				</div>
+				<div className={mini ? "none" : "full-block"}>
+					<div className="overlay">
+						<div className={!miniPlayer ? "box" : "box-mini"}>
+							<div className="button-block">
+								<h3>PLAYER</h3>
+								<CiTurnR1
+									className="button-full"
+									onClick={() => send({ type: "toggle" })}
+								/>
+							</div>
+							<video
+								controls
+								src="../public/14270112_3840_2160_60fps.mp4"
+							></video>
+							<div className="control-panel">
+								{!miniPlayer ? (
+									<CiMinimize1
+										className="button-full"
+										onClick={() =>
+											send({ type: "key.fullMini" })
+										}
+									/>
+								) : (
+									<CiMaximize1
+										className="button-full"
+										onClick={() =>
+											send({ type: "key.full" })
+										}
+									/>
+								)}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/machine.js
+++ src/machine.js
@@ -1,27 +1,38 @@
 import { createMachine } from "xstate";
 
 export const machine = createMachine({
-  id: "player",
-  initial: "mini",
-  states: {
-    mini: {
-      meta: {
-        description: "The video is just a small image; not playing",
-      },
-      on: {
-        toggle: "full",
-      },
-    },
-    full: {
-      entry: "playVideo",
-      exit: "pauseVideo",
-      meta: {
-        description: "The full-screen video",
-      },
-      on: {
-        toggle: "mini",
-        "key.escape": "mini",
-      },
-    },
-  },
+	id: "player",
+	initial: "mini",
+	states: {
+		mini: {
+			meta: {
+				description: "The video is just a small image; not playing",
+			},
+			on: {
+				toggle: "full",
+			},
+		},
+		full: {
+			entry: "playVideo",
+			exit: "pauseVideo",
+			meta: {
+				description: "The full-screen video",
+			},
+			on: {
+				toggle: "mini",
+				"key.escape": "mini",
+				"key.fullMini": "fullMini",
+			},
+		},
+		fullMini: {
+			meta: {
+				description: "The full-mini video",
+			},
+			on: {
+				toggle: "mini",
+				"key.full": "full",
+				"key.escape": "mini",
+			},
+		},
+	},
 });
